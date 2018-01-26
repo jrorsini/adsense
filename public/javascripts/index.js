6c3e31aaf278bf1de90b5290ec0178f78afe4378
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-01-26 11:39:37 +0900

    Added excel4node library.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,40 +11,31 @@ function getAdsenseReport() {
             data:{ date1: date1, date2: date2 }
         }).done((results) => {
             let data = JSON.parse(results);
+            console.log(data);
             $("#result").html(`
-                <p></p>
+                <a href="report.xlsx">Download</a>                
                 <table style="width:100%">
                     <tr>
                         <th>date</th>
-                        <th>earnings</th> 
-                        <th>cost per click</th>
-                        <th>individual add impression rpm</th>
+                        <th>表示回数</th> 
+                        <th>クリック率</th> 
+                        <th>CPC</th>
+                        <th>インプレッション収益</th>
+                        <th>収益</th>
                     </tr>
-                    ${data.rows.map(e => {
-                        if(total[e[0].split('-')[0] + e[0].split('-')[1]] === undefined) {
-                            total[e[0].split('-')[1]] = [Number(e[1]), Number(e[2]), Number(e[3])]
-                        } else {
-                            total[e[0].split('-')[1]][0] += Number(e[1])
-                            total[e[0].split('-')[1]][1] += Number(e[2])
-                            total[e[0].split('-')[1]][2] += Number(e[3])
-                        }
+                    ${data.map(e => {
                         return `
                             <tr>
-                                <td><b>${e[0].split('-')[0]}</b>年<b>${e[0].split('-')[1]}</b>月<b>${e[0].split('-')[2]}</b>日</td>
-                                <td>¥${e[1]}</td>
-                                <td>¥${e[2]}</td>
+                                <td><b>${e[0].split('-')[0]}</b>年<b>${e[0].split('-')[1]}</b>月</td>
+                                <td>${e[1]}</td>
+                                <td>${e[2]}%</td>
                                 <td>¥${e[3]}</td>
+                                <td>¥${e[4]}</td>
+                                <td></td>
                             </tr>
                         `
                     }).join('')}
                 </table>
-            `)
-
-            $("#months").html(`
-                ${Object.values(total).map((e, i) => {
-                    
-                    return `${Object.keys(total)[i]}月 ¥${e[0]} ¥${e[1]} ¥${e[2]} <br/>`
-                })}
             `)
         })
     }
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,15 @@ const google     = require('googleapis');
 const googleAuth = require('google-auth-library');
 const auth       = google.auth
 const adsense    = google.adsense('v1.4');
+const csv        = require('fast-csv');
+const xl         = require('excel4node');
 
+// Create a new instance of a Workbook class
+var wb = new xl.Workbook();
+var ws = wb.addWorksheet('月次');
+ 
+// Add Worksheets to the workbook
+var workSheet = wb.addWorksheet('月次');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -113,22 +121,105 @@ router.post('/adsense', function(req, res, next) {
   function adsenseCallback(auth) {
     console.log("inside adsenseCallback");
 
-    let parameters = {
-      "auth": auth,    
-      "accountId": 'pub-7342164699178968',
-      "dimension" : ["DATE"],
-      "endDate": req.body.date2,
-      "startDate": req.body.date1,
-      "metric"  :["EARNINGS", "COST_PER_CLICK", "INDIVIDUAL_AD_IMPRESSIONS_RPM"]
-    };
-
     adsense.reports.generate(parameters, function(err, response) {
-      console.log(err);
-      console.log(response.data);
-      res.send(JSON.stringify(response.data));
+      console.log(response);
+      let reportData = []
+
+      let date = []
+      let displayNbr = []
+      let clickRatio = []
+      let cpc = []
+      let impressionRevenue = []
+      let revenue = []
+
+      
+
+      response.data.rows.map(e => {
+          if(date.indexOf(e[0]) === -1) date.push(e[0]);
+          if(e[1] === "CPC bids") {
+            cpc.push(e[4])
+            impressionRevenue.push(e[5])
+            clickRatio.push(String(Number(e[6])*100).split('.')[0] + '.' + String(Number(e[6])*100).split('.')[1][0] + String(Number(e[6])*100).split('.')[1][1])
+            displayNbr.push(Math.floor(Number(e[7])))
+          }
+      });
+
+      for(let i = 0; i < date.length; i++) {
+        reportData.push([date[i], displayNbr[i], clickRatio[i], cpc[i], impressionRevenue[i]])
+      }
+
+      let parameters = {
+        "auth": auth,    
+        "accountId": 'pub-7342164699178968',
+        "dimension" : ["MONTH", "BID_TYPE_NAME"],
+        "endDate": req.body.date2,
+        "startDate": req.body.date1,
+        "metric"  :["EARNINGS","CLICKS","COST_PER_CLICK","INDIVIDUAL_AD_IMPRESSIONS_RPM","AD_REQUESTS_CTR", "INDIVIDUAL_AD_IMPRESSIONS"]
+      };
+      var style = wb.createStyle({
+        fill: { type: 'pattern', patternType: 'solid', fgColor: '#CCFFFF'},
+        font: { color: '#000000', size: 12 },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+        border: { bottom: { style: 'thin', color: '#000000' } }
+      });
+  
+      var styleWithoutBorder = wb.createStyle({
+        fill: { type: 'pattern', patternType: 'solid', fgColor: '#CCFFFF'},
+        font: { color: '#000000', size: 12 },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+      });
+  
+      var headerLeftStyle = wb.createStyle({
+        fill: { type: 'pattern', patternType: 'solid', fgColor: '#CCFFFF'},
+        font: { bold: true, color: '#000000', size: 12 },
+        numberFormat: '$#,##0.00; ($#,##0.00); -',
+        border: {
+            left: { style: 'double', color: '#000000' },
+            bottom: { style: 'thin', color: '#000000' }
+        }
+      })
+  
+      ws.cell(1, 2, 1, 11, true).string('Google Travel').style(headerLeftStyle);
+      ws.cell(2, 2, 2, 6, true).string('Adsense').style(
+        wb.createStyle({
+            fill: { type: 'pattern', patternType: 'solid', fgColor: '#CCFFFF'},
+            font: { bold: true, color: '#000000', size: 12 },
+            numberFormat: '$#,##0.00; ($#,##0.00); -',
+            border: { left: { style: 'double', color: '#000000' } }
+        })
+      );
+  
+      ws.cell(2, 7, 2, 11, true).string('AdExchange').style(
+        wb.createStyle({
+            fill: { type: 'pattern', patternType: 'solid', fgColor: '#CCFFFF'},
+            font: { bold: true, color: '#000000', size: 12 },
+            numberFormat: '$#,##0.00; ($#,##0.00); -',
+            border: { left: { style: 'double', color: '#000000' } }
+        })
+      );
+  
+      ws.cell(3,2).string('表示回数').style(headerLeftStyle);
+      ws.cell(3,3).string('クリック率').style(style);
+      ws.cell(3,4).string('CPC').style(style);
+      ws.cell(3,5).string('インプレッション収益').style(style);
+      ws.cell(3,6).string('収益').style(style);
+  
+      ws.cell(3,7).string('広告の表示回数').style(headerLeftStyle);
+      ws.cell(3,8).string('CTR').style(style);
+      ws.cell(3,9).string('CPC').style(style);
+      ws.cell(3,10).string('eCPM').style(style);
+      ws.cell(3,11).string('収益').style(style);
+  
+      ws.row(3).freeze()
+  
+      wb.write('report.xlsx');
+
+      // csv.
+      //   write(reportData, {headers: true})
+      //   .pipe(ws);
+      res.send(JSON.stringify(reportData));
     });
   }
 });
-router.get('https://www.googleapis.com/adsense/v1.4/accounts/pub-7342164699178968/reports')
 
 module.exports = router;
